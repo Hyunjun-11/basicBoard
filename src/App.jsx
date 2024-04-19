cd3f603e025f8fc53f92062d9b6b430183d7fb47
Hyunjun-11 <mical0108@gmail.com>
2024-04-19 21:59:13 +0900

    feat : app component state > reducer

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,63 @@ import Create from "./pages/Create";
 import Home from "./pages/Home";
 import Info from "./pages/Info";
 
-import { useRef, useState } from "react";
+import { useRef, useState, useReducer } from "react";
 import Board from "./exam/Board";
 import SignUp from "./exam/SignUp";
 import Update from "./pages/Update";
 
+const data = [
+  {
+    id: 1,
+    title: "제목",
+    date: "2024-04-04",
+    content: "내용",
+  },
+];
+
+function test(state, action) {
+  switch (action.type) {
+    case "CREATE":
+      return [action.data, ...state];
+    case "UPDATE":
+      return state.map((item) => (item.id === action.id ? action.data : item));
+    case "DELETE":
+      return state.filter((item) => item.id !== action.id);
+    default:
+      return state;
+  }
+}
+
 function App() {
-  const [board, setBoard] = useState([]);
+  const [board, dispatch] = useReducer(test, []);
+
+  const onCreate = (data) => {
+    dispatch({
+      type: "CREATE",
+      data: {
+        id: board.length + 1,
+        title: data.title,
+        content: data.content,
+        date: data.date,
+      },
+    });
+  };
+  const onUpdate = (data) => {
+    dispatch({
+      type: "UPDATE",
+      data: {
+        data,
+      },
+    });
+  };
+
+  const onDelete = (id) => {
+    dispatch({
+      type: "DELETE",
+      data: { id },
+    });
+  };
+
   const [id, setId] = useState();
 
   const boardItemGetId = (id) => {
@@ -18,26 +68,6 @@ function App() {
   };
 
   const boardInfo = board.find((item) => item.id === id);
-
-  const onCreate = (data) => {
-    const newBoard = {
-      id: board.length + 1,
-      title: data.title,
-      content: data.content,
-      date: data.date,
-    };
-    setBoard([newBoard, ...board]);
-  };
-
-  const onUpdate = (id, newData) => {
-    const updateBoard = board.map((item) => (item.id === id ? { ...item, ...newData } : item));
-    setBoard(updateBoard);
-  };
-
-  const onDelete = (id) => {
-    const deleteBoard = board.filter((item) => item.id !== id);
-    setBoard(deleteBoard);
-  };
 
   return (
     <>
